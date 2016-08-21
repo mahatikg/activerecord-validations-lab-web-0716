78fd042014f3e7801de23f5141c96651fc119748
Mahati Gollamudi <github email address>
2016-08-20 21:26:19 -0400

    added custom validations in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait
+
+  def clickbait
+    if self.title && !self.title.match(/Won't Believe|Secret|Guess|Top \d/)
+      errors.add(:title, "not clickbait")
+      false
+    end
+  end
 end
